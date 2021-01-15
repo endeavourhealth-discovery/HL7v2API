b44016101f09af865978ac9c4d62239ffc24babf
prasanthannamreddy <75254787+prasanthannamreddy@users.noreply.github.com>
2021-01-15 10:37:42 +0000

    Update Tables Create.sql
    Changed to match the schema definition, columns to the latest one.

--- Database/Tables Create.sql
+++ Database/Tables Create.sql
@@ -1,22 +1,22 @@
-DROP SCHEMA if exists hl7v2_imperial;
+DROP SCHEMA if exists hl7v2_inbound;
 
-CREATE SCHEMA hl7v2_imperial;
+CREATE SCHEMA hl7v2_inbound;
 
-USE hl7v2_imperial;
+USE hl7v2_inbound;
 
-CREATE TABLE `hl7v2_inbound_data` (
+CREATE TABLE `imperial` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier of the HL7v2 Message',
   `date_received` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'the date the message was received',
   `message_wrapper` text COMMENT 'the information contained in the message wrapper',
   `hl7_message` text COMMENT 'the HL7v2 payload of the message',
   `payload_id` varchar(45) NOT NULL COMMENT 'The unique identifier sent by the publishing system',
-  `processed` boolean not null default FALSE COMMENT 'whether the message has been sent to the messaging API',
+  `send_to_mq`  varchar(1) default 'N' COMMENT 'whether the message has been sent to the messaging API',
   PRIMARY KEY (`id`),
   UNIQUE KEY `payload_id_UNIQUE` (`payload_id`)
 )
 ROW_FORMAT=COMPRESSED
 KEY_BLOCK_SIZE=8;
 
-CREATE INDEX ix_hl7v2_inbound_data_date_received_processed
-  ON hl7v2_inbound_data
-  (date_received, processed);
+CREATE INDEX ix_imperial_date_received_send_to_mq
+  ON imperial
+  (date_received, send_to_mq);
